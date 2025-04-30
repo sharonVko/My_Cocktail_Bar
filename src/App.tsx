09c2af518577f4ed24304fbd6c9a5bdaa46df2c0
sharonVko <shar1.v.kori@gmail.com>
2025-05-01 00:31:32 +0200

    added some folders and installed react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1>Cocktails</h1>
-    </>
+    <div>
+      <Home />
+    </div>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to my Cocktail Bar! Enjoy your drink 🍸</p>
+    </div>
+  );
+};
+
+export default Home;
