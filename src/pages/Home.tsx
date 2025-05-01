8cf0eb2e06753f26264d7fda16dbed0ed3a95f5f
sharonVko <shar1.v.kori@gmail.com>
2025-05-02 00:27:50 +0200

    added outlet and previewcard comp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
-import Header from "../components/header/Header";
+import PreviewCard from "../components/previewCard/PreviewCard";
 
 const Home = () => {
   return (
     <div>
-      <Header />
       <h1>Home</h1>
       <p className="text-amber-700">
         Welcome to my Cocktail Bar! Enjoy your drink 🍸
       </p>
+      <section className="text-center border-2 border-amber-500 shadow-lg p-4 rounded-lg mt-4">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">
+          Cocktail of the Day
+        </h2>
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <PreviewCard />
+        </div>
+      </section>
     </div>
   );
 };
